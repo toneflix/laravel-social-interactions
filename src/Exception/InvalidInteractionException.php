bc7d46bfb54b875e8a8159025d230c1ae9546a3c
3m1n3nc3 <3m1n3nc3@users.noreply.github.com>
2024-07-02 15:14:05 +0000

    Fix styling

--- src/Exception/InvalidInteractionException.php
+++ src/Exception/InvalidInteractionException.php
@@ -12,9 +12,7 @@ use ToneflixCode\SocialInteractions\Traits\HasSocialInteractions;
 final class InvalidInteractionException extends \Exception
 {
     /**
-     *
-     * @param Model|HasSocialInteractions $model
-     * @return self
+     * @param  Model|HasSocialInteractions  $model
      */
     public static function message(Model $model): self
     {
@@ -26,7 +24,7 @@ final class InvalidInteractionException extends \Exception
     public static function invalidReaction(): self
     {
         if (config('social-interactions.enable_reactions')) {
-            return new static(
+            return new self(
                 implode('', [
                     'Invalid reaction, supported reactions include: ',
                     collect(config('social-interactions.available_reactions'))->map(fn ($r, $i) => ($i + 1).": $r")->join(', '),
@@ -34,21 +32,21 @@ final class InvalidInteractionException extends \Exception
             );
         }
 
-        return new static('Invalid Reaction: Send "true" to like or "false" to unlike.');
+        return new self('Invalid Reaction: Send "true" to like or "false" to unlike.');
     }
 
     public static function dislikeDisabled(): self
     {
-        return new static('Dislike are disabled.');
+        return new self('Dislike are disabled.');
     }
 
     public static function saveListDisabled(): self
     {
-        return new static('Saving to lists is disabled.');
+        return new self('Saving to lists is disabled.');
     }
 
     public static function reactionsDisabled(): self
     {
-        return new static('Reactions are disabled.');
+        return new self('Reactions are disabled.');
     }
 }
